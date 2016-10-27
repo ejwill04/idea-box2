4fd73a305a29e2cb0a121a255f1107b93c2115ad
Elijah Williams <ejwill04@gmail.com>
2016-10-27 15:16:44 -0600

    Add search feature among other things

--- idea-box3.js
+++ idea-box3.js
@@ -2,6 +2,7 @@ $(document).ready(function (){
   for (var i=0; i < localStorage.length; i++) {
     var objectIdea = JSON.parse(localStorage.getItem(localStorage.key(i)));
     createIdea(objectIdea);
+    $('.title-input').focus();
   }
 });
 
@@ -19,22 +20,88 @@ function createIdea(idea) {
 }
 
 function Idea(title, body){
- this.title = title;
- this.body = body;
- this.id = Date.now();
- this.quality = 'swill';
+   this.title = title;
+   this.body = body;
+   this.id = Date.now();
+   this.quality = 'swill';
 }
 
+//save feature
 $('.save-button').on('click', function(){
- var title = $('.title-input').val();
- var body = $('.body-input').val();
- var idea = new Idea(title, body);
- createIdea(idea);
- localStorage.setItem(idea.id, JSON.stringify(idea));
+   var title = $('.title-input').val();
+   var body = $('.body-input').val();
+   var idea = new Idea(title, body);
+   createIdea(idea);
+   localStorage.setItem(idea.id, JSON.stringify(idea));
+   clearInput();
+   $('.title-input').focus();
 });
 
-$('.delete-button').on('click', function(){
+//delete feature
+$('.render-idea').on('click', '.delete-button', function(){
   var idNumber = $(this).closest('li').attr('id');
   localStorage.removeItem(idNumber);
-  $('.delete-button').parent.remove();
+  $(this).closest('li').remove();
+});
+
+//quality feature
+$('.render-idea').on('click', '.upvote, .downvote', function() {
+  var parentSelector = $(this).closest('li');
+  var idNumber = parentSelector.attr('id');
+  var quality = parentSelector.find('.quality').text();
+  var newQuality;
+  if ($(this).is('.upvote') ) {
+    newQuality = upVote(quality);
+  } else if ($(this).is('.downvote')) {
+    newQuality = downVote(quality);
+  }
+  changeQualityText(parentSelector, newQuality);
+  storeQuality(idNumber, newQuality);
+});
+
+function changeQualityText (parentSelector, newQuality) {
+  parentSelector.find('.quality').text(newQuality);
+}
+
+function storeQuality (idNumber, newQuality) {
+  var ideaBox = JSON.parse(localStorage.getItem(idNumber));
+  ideaBox.quality = newQuality;
+  localStorage.setItem(idNumber, JSON.stringify(ideaBox));
+}
+
+function upVote (quality) {
+  switch (quality) {
+    case 'swill':
+      return 'plausible';
+    case 'plausible':
+      return 'genius';
+    default:
+      return 'genius';
+  }
+}
+
+function downVote (quality) {
+  switch (quality) {
+    case 'genius':
+      return 'plausible';
+    case 'plausible':
+      return 'swill';
+    default:
+      return 'swill';
+  }
+}
+//end of quality feature
+
+function clearInput () {
+  $('.title-input').val('');
+  $('.body-input').val('');
+}
+
+$('.search-bar').on('keyup', function() {
+  var userSearch = $(this).val().toLowerCase();
+  $('.idea-box').each(function() {
+    var titleText = $(this).find('.title-result').text().toLowerCase();
+    var bodyText = $(this).find('.body-result').text().toLowerCase();
+    titleText.indexOf(userSearch) !== -1 || bodyText.indexOf(userSearch) !== -1 ? $(this).show("slow") : $(this).hide("slow");
+  });
 });
